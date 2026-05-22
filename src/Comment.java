a783876d3ce0fa0055a03c16995a2e73df202c09
Isaac Palomino <isaac.github2806@gmail.com>
2026-05-22 17:29:39 -0500

    "Comentarios y la funcion var"

---
+++ src/Comment.java
@@ -0,0 +1,17 @@
+public class Comment {
+    public static void main(String[] args) {
+        String name, last_name;
+        int age;
+        // declaration the variables
+        name = "Isaac";
+        last_name = "Palomino";
+        age = 17;
+        /*
+        hello world
+        Artemis II
+         */
+        System.out.println("Name: "+name);
+        System.out.println("Last_name: "+last_name);
+        System.out.println("Age: "+age);
+    }
+}
---
+++ src/Var_Java.java
@@ -0,0 +1,9 @@
+public class Var_Java {
+    public static void main(String[] args) {
+        var name = "Isaac";
+        var number = 20;
+        var figure = 10.50;
+
+        System.out.println(name);
+    }
+}

3f104f7d70c2237b35f954ddfdbddb39a206c1f4
Isaac Palomino <isaac.github2806@gmail.com>
2026-05-24 20:17:34 -0500

    "actualizado"

--- src/Increase_Oper.java
+++ src/Increase_Oper.java
@@ -1,37 +1,35 @@
-public class Increase_Oper {
-    public static void main(String[] args) {
+void main() {
 
-        int valor1 = 5;
-        System.out.println("Value original: "+valor1);
+    int valor1 = 5;
+    System.out.println("Value original: " + valor1);
 
-        // Increase value postfix
-        valor1++;
-        System.out.println("Value increase: "+valor1);
+    // Increase value postfix
+    valor1++;
+    System.out.println("Value increase: " + valor1);
 
-        // Increase value prefix
-        ++valor1;
-        System.out.println("Value increase: "+valor1);
+    // Increase value prefix
+    ++valor1;
+    System.out.println("Value increase: " + valor1);
 
-        int value2 = 6;
-        System.out.println("Value original: "+value2);
+    int value2 = 6;
+    System.out.println("Value original: " + value2);
 
-        // Decrement postfix
-        value2--;
-        System.out.println("Value decrement: "+value2);
+    // Decrement postfix
+    value2--;
+    System.out.println("Value decrement: " + value2);
 
-        // Decrement prefix
-        --value2;
-        System.out.println("Value decrement: "+value2);
+    // Decrement prefix
+    --value2;
+    System.out.println("Value decrement: " + value2);
 
-        int value3 = 10;
-        System.out.println("Value original: "+value3);
+    int value3 = 10;
+    System.out.println("Value original: " + value3);
 
-        // Increase operation fixed (multiplication)
-        value3*=2;
-        System.out.println("Value increase: "+value3);
+    // Increase operation fixed (multiplication)
+    value3 *= 2;
+    System.out.println("Value increase: " + value3);
 
-        // Decrement operation fixed (division)
-        value3/=2;
-        System.out.println("Value decrement: "+value3);
-    }
+    // Decrement operation fixed (division)
+    value3 /= 2;
+    System.out.println("Value decrement: " + value3);
 }
